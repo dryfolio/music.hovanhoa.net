26d7da92bd900d8af078b1267561fcdb4108fc7b
hovanhoa <hovanhoaa1@gmail.com>
2026-04-02 12:03:15 +0700

    Add music navbar

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -1,10 +1,11 @@
 const NAME = 'hovanhoa'
 const FULL_NAME = 'Hồ Văn Hòa'
-const BASE_URL = 'https://music.hovanhoa.net'
+const BASE_URL = 'https://hovanhoa.net'
 const TWITTER = '_hovanhoa_'
 const LINKEDIN = 'hovanhoa'
 const GITHUB = 'hovanhoa'
 const IMAGE = '/avatar.png'
+const MUSIC_URL = 'https://music.hovanhoa.net'
 const INSIGHT_URL = 'https://insight.hovanhoa.net'
 const GALLERY_URL = 'https://gallery.hovanhoa.net'
 const ROLE = 'Software Engineer'
@@ -16,6 +17,7 @@ export {
     LINKEDIN,
     GITHUB,
     IMAGE,
+    MUSIC_URL,
     INSIGHT_URL,
     GALLERY_URL,
     BASE_URL,
